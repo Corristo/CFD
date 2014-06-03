685b3f322c578b11fc0ee63095c747bff55ab6f2
Tushar Upadhyay <tusharuiit@gmail.com>
2014-06-03 12:08:08 +0200

    Injection Under construction,result not checked,code compiling only

---
+++ LBM_MPI_tush/parallel.h
@@ -0,0 +1,19 @@
+#include "mpi.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void Program_Message(char *txt);
+/* produces a stderr text output  */
+
+
+
+void Programm_Sync(char *txt);
+/* produces a stderr textoutput and synchronize all processes */
+
+
+
+void Programm_Stop(char *txt);
+/* all processes will produce a text output, be synchronized and finished */
+
+void finalizeMPI();
+void initializeMPI(int * rank,int * number_of_ranks, int argc , char ** argv);
