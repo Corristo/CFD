57b20a20c9a26c1b8161975255c4cbbc554e792b
Xodion <bergler@in.tum.de>
2014-07-07 19:51:37 +0200

    Fixed a major bug with initialization of the flagFields and streaming

--- LBM_mpi/streaming.c
+++ LBM_mpi/streaming.c
@@ -686,10 +686,10 @@ void doStreamingAndCollision(double *collideField, double *streamField,int *flag
         #else
         #pragma omp parallel for private(f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17, f18, density, velocity, tmpFeq0, tmpFeq1, tmpFeq2, dotProduct, x, y, j), firstprivate(numberOfCells, tau), shared(xlength, streamField, collideField) schedule(static)
         #endif
-        for (z = 2 * xlength[2] / 3 + 1; z <= xlength[2] - 1; z++)
+        for (z = 2 * xlength[2] / 3 + 1; z <= xlength[2]; z++)
         {
-            j = xlength[0] + 3 + (xlength[0] + 2) * (xlength[1] + 2) * z;
-            for (y = 1; y <= xlength[1]; y++)
+            j = 1 + 2 * (xlength[0] + 2) + (xlength[0] + 2) * (xlength[1] + 2) * z;
+            for (y = 2; y <= xlength[1] - 1; y++)
             {
                 for (x = 1; x <= xlength[0]; x++)
                 {
@@ -1003,7 +1003,7 @@ void doStreamingAndCollision(double *collideField, double *streamField,int *flag
                 }
                 j += 2; // skip the last two cells of the current row and the first two cell of the next row
             }
-            j += 2 * xlength[0] + 4;
+            j += 4 * xlength[0] + 8;
         }
         break;
     case 4:
@@ -2573,9 +2573,9 @@ void doStreamingAndCollision(double *collideField, double *streamField,int *flag
             j += 2 * xlength[0] + 6;
         }
 
-        /** xlength[2] / 3 < z <= 2 * xlength[2] / 3 */
+        /** xlength[2] / 3 < z <= xlength[2] */
         #pragma omp for schedule(static)
-        for (z = xlength[2] / 3 + 1; z <= 2 * xlength[2] / 3; z++)
+        for (z = xlength[2] / 3 + 1; z <= xlength[2]; z++)
         {
             // y = 1
             j = (xlength[0] + 2) * (xlength[1] + 2) * z + (xlength[0] + 2) + 1;
@@ -3197,322 +3197,6 @@ void doStreamingAndCollision(double *collideField, double *streamField,int *flag
             }
             j += 2 * xlength[0] + 6;
         }
-        /** z = xlength[2] */
-
-        #pragma omp for schedule(static)
-        for (y = 1; y <= xlength[1]; y++)
-        {
-            j = 1 + (xlength[0] + 2) * (xlength[1] + 2) * (xlength[2]) + y * (xlength[0] + 2);
-            for (x = 1; x <= xlength[0]; x++)
-            {
-                density = velocity[0] = velocity[1] = velocity[2] = 0;
-                    #ifdef _ARBITRARYGEOMETRY_
-                    streamMask = !flagField[j];
-                    copyMask = !streamMask;
-                    #endif // _ARBITRARYGEOMETRY_
-                    // load direction i = 0;
-                    // c_0 = (0, -1, -1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f0 = collideField[j + (xlength[0] + 2) * (xlength[1] + 3)] * streamMask + copyMask * collideField[j];
-                    #else
-                    f0 = collideField[j + (xlength[0] + 2) * (xlength[1] + 3)];
-                    #endif
-                    density += f0;
-                    velocity[1] -= f0;
-                    velocity[2] -= f0;
-
-                    // load direction i = 1;
-                    // c_1 = (-1, 0, -1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f1 = collideField[numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) + 1] * streamMask + copyMask * collideField[j + numberOfCells];
-                    #else
-                    f1 = collideField[numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) + 1];
-                    #endif
-
-                    density += f1;
-                    velocity[0] -= f1;
-                    velocity[2] -= f1;
-
-                    // load direction i = 2;
-                    // c_2 = (0, 0, -1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f2 = collideField[2 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2)] * streamMask + copyMask * collideField[j + numberOfCells * 2];
-                    #else
-                    f2 = collideField[2 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2)];
-                    #endif
-
-                    density += f2;
-                    velocity[2] -= f2;
-
-                    // load direction i = 3;
-                    // c_3 = (1, 0, -1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f3 = collideField[3 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) - 1] * streamMask + copyMask * collideField[j + numberOfCells * 3];
-                    #else
-                    f3 = collideField[3 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) - 1];
-                    #endif
-
-                    density += f3;
-                    velocity[0] += f3;
-                    velocity[2] -= f3;
-
-                    // load direction i = 4;
-                    // c_4 = (0, 1, -1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f4 = collideField[4 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) - (xlength[0] + 2)] * streamMask + copyMask * collideField[j + numberOfCells * 4];
-                    #else
-                    f4 = collideField[4 * numberOfCells + j + (xlength[0] + 2) * (xlength[1] + 2) - (xlength[0] + 2)];
-                    #endif
-
-                    density += f4;
-                    velocity[1] += f4;
-                    velocity[2] -= f4;
-
-                    // load direction i = 5;
-                    // c_5 = (-1, -1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f5 = collideField[5 * numberOfCells + j + xlength[0] + 3] * streamMask + copyMask * collideField[j + numberOfCells * 5];
-                    #else
-                    f5 = collideField[5 * numberOfCells + j + xlength[0] + 3];
-                    #endif
-
-                    density += f5;
-                    velocity[0] -= f5;
-                    velocity[1] -= f5;
-
-                    // load direction i = 6;
-                    // c_6 = (0, -1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f6 = collideField[6 * numberOfCells + j + xlength[0] + 2] * streamMask + copyMask * collideField[j + numberOfCells * 6];
-                    #else
-                    f6 = collideField[6 * numberOfCells + j + xlength[0] + 2];
-                    #endif
-
-                    density += f6;
-                    velocity[1] -= f6;
-
-                    // load direction i = 7;
-                    // c_7 = (1, -1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f7 = collideField[7 * numberOfCells + j + xlength[0] + 1] * streamMask + copyMask * collideField[j + numberOfCells * 7];
-                    #else
-                    f7 = collideField[7 * numberOfCells + j + xlength[0] + 1];
-                    #endif
-
-                    density += f7;
-                    velocity[0] += f7;
-                    velocity[1] -= f7;
-
-                    // load direction i = 8;
-                    // c_8 = (-1, 0, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f8 = collideField[8 * numberOfCells + j + 1] * streamMask + copyMask * collideField[j + numberOfCells * 8];
-                    #else
-                    f8 = collideField[8 * numberOfCells + j + 1];
-                    #endif
-
-                    density += f8;
-                    velocity[0] -= f8;
-
-                    // load direction i = 9;
-                    // c_9 = (0, 0, 0)
-                    f9 = collideField[9 * numberOfCells + j];
-                    density += f9;
-
-                    // load direction i = 10;
-                    // c_10 = (1, 0, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f10 = collideField[10 * numberOfCells + j - 1] * streamMask + copyMask * collideField[j + numberOfCells * 11];
-                    #else
-                    f10 = collideField[10 * numberOfCells + j - 1];
-                    #endif
-
-                    density += f10;
-                    velocity[0] += f10;
-
-                    // load direction i = 11;
-                    // c_11 = (-1, 1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f11 = collideField[11 * numberOfCells + j - xlength[0] - 1] * streamMask + copyMask * collideField[j + numberOfCells * 11];
-                    #else
-                    f11 = collideField[11 * numberOfCells + j - xlength[0] - 1];
-                    #endif
-
-                    density += f11;
-                    velocity[0] -= f11;
-                    velocity[1] += f11;
-
-                    // load direction i = 12;
-                    // c_12 = (0, 1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f12 = collideField[12 * numberOfCells + j - xlength[0] - 2] * streamMask + copyMask * collideField[j + numberOfCells * 12];
-                    #else
-                    f12 = collideField[12 * numberOfCells + j - xlength[0] - 2];
-                    #endif
-
-                    density += f12;
-                    velocity[1] += f12;
-
-                    // load direction i = 13;
-                    // c_13 = (1, 1, 0)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f13 = collideField[13 * numberOfCells + j - xlength[0] - 3] * streamMask + copyMask * collideField[j + numberOfCells * 13];
-                    #else
-                    f13 = collideField[13 * numberOfCells + j - xlength[0] - 3];
-                    #endif
-
-                    density += f13;
-                    velocity[0] += f13;
-                    velocity[1] += f13;
-
-                    // load direction i = 14;
-                    // c_14 = (0, -1, 1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f14 = collideField[14 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 1)] * streamMask + copyMask * collideField[j + numberOfCells * 14];
-                    #else
-                    f14 = collideField[14 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 1)];
-                    #endif
-
-                    density += f14;
-                    velocity[1] -= f14;
-                    velocity[2] += f14;
-
-                    // load direction i = 15;
-                    // c_15 = (-1, 0, 1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f15 = collideField[15 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2) + 1] * streamMask + copyMask * collideField[j + numberOfCells * 15];
-                    #else
-                    f15 = collideField[15 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2) + 1];
-                    #endif
-
-                    density += f15;
-                    velocity[0] -= f15;
-                    velocity[2] += f15;
-
-                    // load direction i = 16;
-                    // c_16 = (0, 0, 1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f16 = collideField[16 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2)] * streamMask + copyMask * collideField[j + numberOfCells * 16];
-                    #else
-                    f16 = collideField[16 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2)];
-                    #endif
-
-                    density += f16;
-                    velocity[2] += f16;
-
-                    // load direction i = 17;
-                    // c_17 = (1, 0, 1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f17 = collideField[17 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2) - 1] * streamMask + copyMask * collideField[j + numberOfCells * 17];
-                    #else
-                    f17 = collideField[17 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 2) - 1];
-                    #endif
-
-                    density += f17;
-                    velocity[0] += f17;
-                    velocity[2] += f17;
-
-                    // load direction i = 18;
-                    // c_18 = (0, 1, 1)
-                    #ifdef _ARBITRARYGEOMETRY_
-                    f18 = collideField[18 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 3)] * streamMask + copyMask * collideField[j + numberOfCells * 18];
-                    #else
-                    f18 = collideField[18 * numberOfCells + j - (xlength[0] + 2) * (xlength[1] + 3)];
-                    #endif
-
-                    density += f18;
-                    velocity[1] += f18;
-                    velocity[2] += f18;
-
-                velocity[0] = velocity[0] / density;
-                velocity[1] = velocity[1] / density;
-                velocity[2] = velocity[2] / density;
-
-                // compute feq and relaxate; then store to streamField
-                tmpFeq0 = 1 - (velocity[0] * velocity[0] + velocity[1] * velocity[1] + velocity[2] * velocity[2] ) / (2 * C_S * C_S);
-
-                // i = 0 and i = 18
-                // c_0 = (0, -1, -1), c_18 = (0, 1, 1), w_0 = w_18 = 1/36
-                dotProduct = - velocity[1] - velocity[2];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[j] = (1 - 1./tau) * f0 + 1./tau * tmpFeq1;
-                streamField[18 * numberOfCells + j] = (1 - 1./tau) * f18 + 1./tau * tmpFeq2;
-
-                // i = 1 and i = 17
-                // c_1 = (-1, 0, -1), c_17 = (1, 0, 1), w_1 = w_17 = 1/36
-                dotProduct = - velocity[0] - velocity[2];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[numberOfCells + j] = (1 - 1./tau) * f1 + 1./tau * tmpFeq1;
-                streamField[17 * numberOfCells + j] = (1 - 1./tau) * f17 + 1./tau * tmpFeq2;
-
-                // i = 2 and i = 16
-                // c_2 = (0, 0, -1), c_16 = (0, 0, 1), w_2 = w_16 = 1/18
-                dotProduct = - velocity[2];
-                tmpFeq1 = 1./18 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./18 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[2 * numberOfCells + j] = (1 - 1./tau) * f2 + 1./tau * tmpFeq1;
-                streamField[16 * numberOfCells + j] = (1 - 1./tau) * f16 + 1./tau * tmpFeq2;
-
-                // i = 3 and i = 15
-                // c_3 = (1, 0, -1), c_16 = (-1, 0, 1), w_3 = w_15 = 1/36
-                dotProduct = velocity[0] - velocity[2];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[3 * numberOfCells + j] = (1 - 1./tau) * f3 + 1./tau * tmpFeq1;
-                streamField[15 * numberOfCells + j] = (1 - 1./tau) * f15 + 1./tau * tmpFeq2;
-
-                // i = 4 and i = 14
-                // c_4 = (0, 1, -1), c_14 = (0, -1, 1), w_4 = w_14 = 1/36
-                dotProduct = velocity[1] - velocity[2];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[4 * numberOfCells + j] = (1 - 1./tau) * f4 + 1./tau * tmpFeq1;
-                streamField[14 * numberOfCells + j] = (1 - 1./tau) * f14 + 1./tau * tmpFeq2;
-
-                // i = 5 and i = 13
-                // c_5 = (-1, -1, 0), c_13 = (1, 1, 0), w_5 = w_13 = 1/36
-                dotProduct = - velocity[0] - velocity[1];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[5 * numberOfCells + j] = (1 - 1./tau) * f5 + 1./tau * tmpFeq1;
-                streamField[13 * numberOfCells + j] = (1 - 1./tau) * f13 + 1./tau * tmpFeq2;
-
-                // i = 6 and i = 12
-                // c_6 = (0, -1, 0), c_14 = (0, 1, 0), w_6 = w_12 = 1/18
-                dotProduct = - velocity[1];
-                tmpFeq1 = 1./18 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./18 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[6 * numberOfCells + j] = (1 - 1./tau) * f6+ 1./tau * tmpFeq1;
-                streamField[12 * numberOfCells + j] = (1 - 1./tau) * f12 + 1./tau * tmpFeq2;
-
-                // i = 7 and i = 11
-                // c_7 = (1, -1, 0), c_11 = (-1, 1, 0), w_7 = w_11 = 1/36
-                dotProduct = velocity[0] - velocity[1];
-                tmpFeq1 = 1./36 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./36 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[7 * numberOfCells + j] = (1 - 1./tau) * f7 + 1./tau * tmpFeq1;
-                streamField[11 * numberOfCells + j] = (1 - 1./tau) * f11 + 1./tau * tmpFeq2;
-
-                // i = 8 and i = 10
-                // c_8 = (-1, 0, 0), c_10 = (1, 0, 0), w_8 = w_10 = 1/18
-                dotProduct = - velocity[0];
-                tmpFeq1 = 1./18 * density * (tmpFeq0 + dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                tmpFeq2 = 1./18 * density * (tmpFeq0 - dotProduct/(C_S * C_S) + dotProduct * dotProduct / (2 * C_S * C_S * C_S * C_S));
-                streamField[8 * numberOfCells + j] = (1 - 1./tau) * f8 + 1./tau * tmpFeq1;
-                streamField[10 * numberOfCells + j] = (1 - 1./tau) * f10 + 1./tau * tmpFeq2;
-
-                // i = 9
-                // c_9 = (0, 0, 0), w_9 = 1/3
-                streamField[9 * numberOfCells + j] = (1 - 1. / tau) * f9 + 1. / (3 * tau) * density * tmpFeq0;
-
-
-                j++;
-            }
-            j += 2; // skip the last two cells of the current row and the first two cell of the next row
-        }
-
     }
     break;
     }
